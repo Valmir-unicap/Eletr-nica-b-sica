492f8033c46a46a24ffeb168ee0987f709f44be6
Valmir Júnior <61609240+Valmir-unicap@users.noreply.github.com>
2021-04-22 05:44:02 -0300

    Create Pisca pisca.c

---
+++ Pisca pisca.c
@@ -0,0 +1,10 @@
+void setup() {
+  pinMode(11,OUTPUT);//defini a porta do led como saída
+}
+
+void loop() {
+    digitalWrite(11,HIGH);//Acender o led
+    delay(1000);//Intervalo de tempo em milissegundo
+    digitalWrite(11,LOW);//Apagar o led
+    delay(1000);//intervalo de tempo em milissegundo
+}
