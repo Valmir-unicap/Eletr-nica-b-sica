06f16d2d04c8690025a65a52b31f9673d27e0dcd
Valmir Júnior <61609240+Valmir-unicap@users.noreply.github.com>
2021-04-22 05:46:30 -0300

    O código segue as seguintes exigências, abaixo:
    Pisca pisca com um led, usando:
    - Placa protoboard
    - Fios
    - Resistor
    - Fonte
    - Led

--- Pisca pisca.c
+++ Pisca pisca.c
@@ -1,7 +1,6 @@
 void setup() {
   pinMode(11,OUTPUT);//defini a porta do led como saída
 }
-
 void loop() {
     digitalWrite(11,HIGH);//Acender o led
     delay(1000);//Intervalo de tempo em milissegundo
