264c484bebe13a60dec62a71e7c1dc52d31ea2a0
Valmir Júnior <61609240+Valmir-unicap@users.noreply.github.com>
2021-04-22 05:40:55 -0300

    O código segue as seguintes exigências, abaixo:
    Acender um led, usando:
    - Placa protoboard
    - Fios
    - Resistor
    - Fonte
    - Led

--- Acender um led.c
+++ Acender um led.c
@@ -1,7 +1,8 @@
-void setup(){
-  pinMode(11,OUTPUT){//defini a porta do led como saída
-  }
-  void lopp(){
-    digitalWrite(11,High);//Acender o led
+void setup() {
+  pinMode(11,OUTPUT);//defini a porta do led como saída
+}
+
+void loop() {
+    digitalWrite(11,HIGH);//Acender o led
     delay(1000);//Intervalo de tempo em milissegundo
-  }
+}
